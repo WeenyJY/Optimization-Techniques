8db356c90c2efc56d2ae6c8041726dda2327dfb3
unknown <kosletr@gmail.com>
2019-12-21 11:55:13 +0200

    Started Work 3

--- Εργασία 3/Ergasia3.m
+++ Εργασία 3/Ergasia3.m
@@ -1,7 +1,7 @@
 % Konstantinos Letros 8851
 % Optimization Techniques
-% Project 3 - Compariston to Constrained Gradient Descent Method
-% Steepest Descent - Steepest Descent with Projection
+% Project 3 -
+%
 
 
 %% Clean the screen
@@ -12,111 +12,51 @@ close all;
 format long;
 
 %% Parameters
-
+e = 1e-4;
 plotNum = 0;
 
 % Inital Conditions
-x_1 = [8,-5, 11];
-y_1 = [3, 7,  3];
+x_1 = 5;
+y_1 = -10;
 
-% Gamma Parameters
-gamma_A = [0.1,1,2,10];
-gamma_B = [0.1,0.3,0.01];
+% Boundaries
+a1 = 3;
+b1 = 30;
+a2 = -25;
+b2 = -5;
 
-% Accuracy Parameters
-e_A = [0.01,0.01,0.01,0.01];
-e_B = [0.01,0.02,0.01];
-
-% Projection Parameters
-s = [15,1,0.1];
-a1 = -20;
-b1 =  10;
-a2 = -12;
-b2 =  15;
-
-%% Steepest Descent - Part A (a)
+%% Steepest Descent
 fprintf("####### STEEPEST DESCENT #######\n\n")
-partB = 0;
 
-for i = 1 : 4
-    
-    % Count Number of Plots
-    plotNum = plotFunction(plotNum,partB,a1,b1,a2,b2);
-    
-    % For all Initial Conditions
-    for i=1:length(x_1)
-        
-        fprintf("Initial Conditions [x,y] =  [%f,%f]\n",x_1(i),y_1(i));
-        
-        fprintf("Gamma = %f , Accuracy e = %f \n", gamma_A(i) ,  e_A(i) )
-        
-        % Steepest Descent
-        [x,y,k] = steepestDescent(x_1(i),y_1(i),e_A(i),gamma_A(i));
-        
-        if k < 101
-            fprintf("Min(f) = %f at [x,y] = [%f,%f] after %d repetitions\n\n", ...
-                f(x(end),y(end)),x(end),y(end),k);
-        end
-        
-        % Plot Trace
-        tracePlot (x,y,k,plotNum-1)
-        tracePlot (x,y,k,plotNum)
-    end
-    
-    figure(plotNum-1)
-    title(['3D Plot - Steepest Descent - Gamma = ',num2str( gamma_A(i) ),' - Accuracy e = ',num2str(e_A(i))])
-    xlabel("x")
-    ylabel("y")
-    zlabel("f(x,y)")
-    
-    figure(plotNum)
-    title(['2D Plot - Steepest Descent - Gamma = ',num2str( gamma_A(i) ),' - Accuracy e = ',num2str(e_A(i))])
-    xlabel("x")
-    ylabel("y")
-    zlabel("f(x,y)")
+% Count Number of Plots
+plotNum = plotFunction(plotNum,a1,b1,a2,b2);
+
+fprintf("Initial Conditions [x,y] =  [%f,%f]\n",x_1,y_1);
+
+% Steepest Descent
+[x,y,k] = steepestDescent(x_1,y_1,e,a1,b1,a2,b2);
+
+if k < 101
+    fprintf("Min(f) = %f at [x,y] = [%f,%f] after %d repetitions\n\n", ...
+        f(x(end),y(end)),x(end),y(end),k);
 end
 
-%% Steepest Descent with Projection - Part B (b,c,d)
-fprintf("\n\n####### STEEPEST DESCENT with Projection #######\n\n")
-partB = 1;
+% Plot Trace
+tracePlot (x,y,k,plotNum-1)
+tracePlot (x,y,k,plotNum)
 
-for i = 1 : 3
-    
-    % Count Number of Plots
-    plotNum = plotFunction(plotNum,partB,a1,b1,a2,b2);
-    
-    fprintf("Initial Conditions [x,y] =  [%f,%f]\n",x_1(i),y_1(i));
-    
-    fprintf("Gamma = %f , Accuracy e = %f , s = %f \n", gamma_B(i),  e_B(i), s(i) )
-    
-    % Steepest Descent
-    [x,y,k] = steepestDescentProj(x_1(i),y_1(i),e_B(i),gamma_B(i),s(i),a1,b1,a2,b2);
-    
-    if k < 101
-        fprintf("Min(f) = %f at [x,y] = [%f,%f] after %d repetitions\n\n", ...
-            f(x(end),y(end)),x(end),y(end),k);
-    end
-    
-    % Plot Trace
-    tracePlot (x,y,k,plotNum-1)
-    tracePlot (x,y,k,plotNum)
-    
-    
-    figure(plotNum-1)
-    title(['3D Plot - Steepest Descent with Projection - Gamma = ',num2str( gamma_B(i) ),...
-        ' - Accuracy e = ',num2str(e_B(i)),' - s_k = ',num2str(s(i))])
-    
-    xlabel("x")
-    ylabel("y")
-    zlabel("f(x,y)")
-    
-    figure(plotNum)
-    title(['2D Plot - Steepest Descent with Projection - Gamma = ',num2str( gamma_B(i) ),...
-        ' - Accuracy e = ',num2str(e_B(i)),' - s_k = ',num2str(s(i))])
-    xlabel("x")
-    ylabel("y")
-    zlabel("f(x,y)")
-end
+
+figure(plotNum-1)
+title('3D Plot - Steepest Descent - Optimized Gamma')
+xlabel("x")
+ylabel("y")
+zlabel("f(x,y)")
+
+figure(plotNum)
+title('2D Plot - Steepest Descent - Optimized Gamma')
+xlabel("x")
+ylabel("y")
+zlabel("f(x,y)")
 
 %% Save Plots
 
@@ -131,132 +71,140 @@ end
 %% Functions
 
 % Objective Function
-
 function res = f(x,y)
 
-res = 1/2*(x.^2+y.^2);
+res = x.*y+2*(x-y).^2;
 
 end
 
-% Derivative of the Objective Function
+% Beta function
+function res = Beta(x,y,a1,b1,a2,b2)
 
-function res = gradf(x,y)
+h{1} = @(x,y) a1-x;
+h{2} = @(x,y) x-b1;
+h{3} = @(x,y) a2-y;
+h{4} = @(x,y) y-b2;
 
-res = [x ; y];
+F = @(x,y,h) -1/h; 
+
+res = 0;
+for i = 1:4
+    res = res + F(x,y,h{i}(x,y));
+end
+
+end
+
+% Auxiliary Function 
+function res = phi(x,y,a1,a2,b1,b2)
+
+r_k = 0.5;
+res = f(x,y)+r_k*Beta(x,y,a1,b1,a2,b2);
+
+end
+
+% Derivative of the Auxiliary Function
+function res = gradphi(x,y)
+
+res = 
 
 end
 
 % Plot given Function
-function plotNum = plotFunction(plotNum,partB,a1,b1,a2,b2)
+function plotNum = plotFunction(plotNum,a1,b1,a2,b2)
 
-
-x = linspace(-21, 21, 100);
-y = linspace(-21, 21, 100);
+x =a1-3:0.1:b1+3;
+y =a2-3:0.1:b2+3;
 
 [X,Y] = meshgrid(x,y);
 
 func = [];
-for i = 1:length(x)
-    for j = 1:length(y)
+for i = 1:length(y)
+    for j = 1:length(x)
         func(i,j) = f(X(i,j),Y(i,j));
     end
 end
 
-
-
-figure(plotNum+1)
+figure(plotNum + 1)
 surf(X,Y,func)
 view(-10,25)
 colorbar
-if partB == 1
-     t = linspace(-50,500,1000);
-     line(a1*ones(size(t)),a2*ones(size(t)),t,'linewidth',3,'Color','#EDB120','LineStyle','--');
-     line(a1*ones(size(t)),b1*ones(size(t)),t,'linewidth',3,'Color','#EDB120','LineStyle','--');
-     line(b1*ones(size(t)),b2*ones(size(t)),t,'linewidth',3,'Color','#EDB120','LineStyle','--');
-     line(b1*ones(size(t)),a2*ones(size(t)),t,'linewidth',3,'Color','#EDB120','LineStyle','--');
-end
+
+t = -1e+3:10:1e+4;
+line(a1*ones(size(t)),a2*ones(size(t)),t,'linewidth',3,'Color','#EDB120','LineStyle','--');
+line(a1*ones(size(t)),b1*ones(size(t)),t,'linewidth',3,'Color','#EDB120','LineStyle','--');
+line(b1*ones(size(t)),b2*ones(size(t)),t,'linewidth',3,'Color','#EDB120','LineStyle','--');
+line(b1*ones(size(t)),a2*ones(size(t)),t,'linewidth',3,'Color','#EDB120','LineStyle','--');
 
 figure(plotNum + 2)
 contour(X,Y,func,20)
 colorbar
-if partB == 1
-    rectangle('Position',[a1 a2 b1-a1 b2-a2],'linewidth',3,'EdgeColor','#EDB120','LineStyle','--');
-end
+rectangle('Position',[a1 a2 b1-a1 b2-a2],'linewidth',3,'EdgeColor','#EDB120','LineStyle','--');
 
-plotNum = plotNum + 2;
+plotNum = plotNum + 3;
 
 end
 
-function [x,y,k] = steepestDescent(x,y,e,gammaMethod)
+% Steepest Descent Method
+function [x,y,k] = steepestDescent(x,y,e,a1,b1,a2,b2)
 k = 1;
 d = [];
 gamma = [];
 
-
-while( norm( gradf(x(k),y(k)) ) >= e)
+while( norm( gradphi(x(k),y(k)) ) >= e)
     
-    d(:,k) = - gradf(x(k),y(k));
-    gamma(k) = gammaMethod;
+    d(:,k) = - gradphi(x(k),y(k));
+    gamma(k) = calcGamma(x(k),y(k),d(:,k),a1,b1,a2,b2);
     x(k+1) = x(k) + gamma(k)*d(1,k);
     y(k+1) = y(k) + gamma(k)*d(2,k);
     
     k = k + 1;
-    
-    if(k>5000)
-        fprintf("Diverging...\n\n");
-        break;
+    if k>50000
+        fprintf("Algorithm did not converge\n")
+        break
     end
-    
 end
 
 end
+function gamma = calcGamma(x_k, y_k, d_k,a1,b1,a2,b2)
 
+% function to be minimized with respect to gamma
+func = @(gamma) phi(x_k+gamma*d_k(1) , y_k+gamma*d_k(2) ,a1,b1,a2,b2);
 
-function [x,y,k] = steepestDescentProj(x,y,e,gammaMethod,s,a1,b1,a2,b2)
+% Golden Section Parameters
+l = 1e-3;
+a = 1e-4;
+b = 2;
+
+% Minimization using Golden Section Method
+gamma = goldenSectionMethod(func,l,a,b);
+
+end
+
+function minX = goldenSectionMethod(func,l,a,b)
+
+gamma = (-1+sqrt(5))/2;
 k = 1;
-x_bar = [];
-y_bar = [];
-gamma = [];
+x1(k)=a(k)+(1-gamma)*(b(k)-a(k));
+x2(k)=a(k)+gamma*(b(k)-a(k));
 
-while( norm( gradf(x(k),y(k)) ) >= e)
+while b(k)-a(k)>=l
     
-    gamma(k) = gammaMethod;
-    
-    x_bar(k) = calcProj(s,x(k),y(k),a1,b1,1);
-    y_bar(k) = calcProj(s,x(k),y(k),a2,b2,2);
-    
-    x(k+1) = x(k) + gamma(k)*(x_bar(k)-x(k));
-    y(k+1) = y(k) + gamma(k)*(y_bar(k)-y(k));
-    
+    if(func(x1(k))<=func(x2(k)))
+        a(k+1)= a(k);
+        b(k+1) = x2(k);
+        x1(k+1) = a(k+1) + (1-gamma)*(b(k+1)-a(k+1));
+        x2(k+1) = x1(k);
+    else
+        a(k+1) = x1(k);
+        b(k+1)= b(k);
+        x1(k+1) = x2(k);
+        x2(k+1) = a(k+1) + gamma*(b(k+1)-a(k+1));
+    end
     k = k + 1;
-    
-      if k>5000
-        fprintf("Diverging...\n\n");
-        break;
-      end
-    
 end
 
-end
-
-function proj = calcProj(s,x_k,y_k,a,b,index)
-
-if index == 1
-    vec_k = x_k;
-elseif index == 2
-    vec_k = y_k;
-else
-    fprintf("ERROR")
-end
-
-if vec_k <= a
-    proj = a;
-elseif vec_k >= b
-    proj = b;
-else
-    grad = gradf(x_k,y_k);
-    proj = vec_k-s*grad(index);
-end
+% Minimum point in the center of the final interval
+minX = (a(end)+b(end))/2;
 
 end
 
@@ -280,7 +228,6 @@ plot3(x(end),y(end),f(x(end),y(end)),"-r*",'linewidth',7)
 end
 
 % Function to automatically save plots in high resolution
-
 function savePlot(name)
 
 % Resize current figure to fullscreen for higher resolution image
